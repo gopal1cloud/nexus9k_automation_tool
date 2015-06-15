b47ef9e016c1f56335ade6c67acd3c3bea9f70b3
Gopalasivam <gopal@onecloudinc.com>
2015-06-16 04:06:04 +0530

    Modified settings file

--- nexus9k_automation_tool/settings.py
+++ nexus9k_automation_tool/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '9ndmu97*$av6)ldrqnwy_2gw-thf%55@-hlbzz6p#izba(hsel'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
