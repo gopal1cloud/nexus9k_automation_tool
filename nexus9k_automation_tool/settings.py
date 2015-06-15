f8b322a0818be679523750c3a83eebfe19e44d1b
Gopalasivam <gopal@onecloudinc.com>
2015-06-15 23:57:38 +0530

    Updated download code without opening in new tab

--- nexus9k_automation_tool/settings.py
+++ nexus9k_automation_tool/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '9ndmu97*$av6)ldrqnwy_2gw-thf%55@-hlbzz6p#izba(hsel'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
