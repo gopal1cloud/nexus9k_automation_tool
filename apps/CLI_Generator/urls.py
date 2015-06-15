6fb26d940ba45c775513f3e69afe1314652aecb6
Gopalasivam <gopal@onecloudinc.com>
2015-06-16 02:52:30 +0530

    Added functionality for ansible automation with  input parameters

--- apps/CLI_Generator/urls.py
+++ apps/CLI_Generator/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('apps.CLI_Generator.views',
     url(r'^convert/$', 'convert'), 
     url(r'^nexus_config_csv/download/$', 'download_nexus_config_csv'), 
     url(r'^nexus_config_generator/download/$', 'download_nexus_config_generator'), 
-    url(r'^nexus_config_puppet/convert/$', 'convert_nexus_config_puppet'), 
+    url(r'^nexus_config_puppet/convert/$', 'convert_nexus_config_puppet'),  
+    url(r'^nexus_config_ansible/convert/$', 'convert_nexus_config_ansible'), 
     url(r'^convert/$', 'convert'), 
 )
--- apps/CLI_Generator/views.py
+++ apps/CLI_Generator/views.py
@@ -55,35 +55,26 @@ def convert_nexus_config_puppet(request):
 	else:
 		return HttpResponseForbidden()
 				
-def convert(request):
+def convert_nexus_config_ansible(request):
 	if request.method == "GET":
-		comments = []
-		code_desc = request.GET["code_desc"]
-		code_host = request.GET["code_host"]
-		code_filename = request.GET["code_filename"]
-		code_file_content = request.GET["code_file_content"]
+		commands = []
+		code_desc = request.GET["ansible_code_desc"]
+		code_host = request.GET["ansible_code_host"]
+		code_filename = request.GET["ansible_code_filename"]
+		code_file_content = request.GET["ansible_code_file_content"]
 		file_data = code_file_content.replace(r"\r\n", r"\n")
 		formated_data = str("\n".join(file_data.splitlines()))
 
 		for line in formated_data.split('\n'):
 			li = line.strip()
-			if li.startswith("!") or li.startswith("#"):
-				comments.append(li)
-
-		for line in formated_data.split('\n'):
-			if line.startswith("!") or line.startswith("#"):
-				sections.append([])
-			elif sections and line:
-				sections[-1].append(line)
+			commands.append(li)
      
 		yaml_file ='---'+'\n'
 		yaml_file +='# To run this Ansible playbook, issue the following command on the Ansible server:\n#\n# ansible-playbook -i hosts <filename>\n#\n'
 		yaml_file +='\n\n\n'+'- name: '+code_desc+'\n'
 		yaml_file +='  hosts: '+code_host+'\n\n'
 		yaml_file +='  tasks: '+'\n\n'
-		for c,s in zip(comments,sections):
-			repl = c.replace("!","#")
-			yaml_file += str(repl)+'\n'+'  - nxos_command:'+'\n'+'      host: "{{ inventory_hostname }}"'+'\n'+'      type: config'+'\n'+"      command: "+str(s)+"\n\n"
+		yaml_file +='  - nxos_command:'+'\n'+'      host: "{{ inventory_hostname }}"'+'\n'+'      type: config'+'\n'+"      command: "+str(commands)+"\n\n"
 
 		response = HttpResponse(content_type='text/x-yaml')
 		response['Content-Disposition'] = 'attachment; filename="'+code_filename+'.yml"'
